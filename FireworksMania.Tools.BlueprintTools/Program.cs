715964e00085d0256935d168bf220bcd3c996e0e
Laumania <mads@laumania.net>
2021-05-11 17:28:00 +0200

    Initial solution

---
+++ FireworksMania.Tools.BlueprintTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FireworksMania.Tools.BlueprintTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
